84241b71325394897fc17a5665c5b1e2195437bc
Yulia <seminajlj@rambler,ru>
2021-06-22 22:58:46 +0300

    Secong commit

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] postes;
+
+    public Post[] search(int ownerId, int domain, String query, boolean ownersOnly, int count, int offset){
+        return null;
+    }
+    public void delete(int ownerId, int postId){
+
+    }
+}
